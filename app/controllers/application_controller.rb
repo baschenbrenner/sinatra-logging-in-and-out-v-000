6262b64f12756224d41300c438eeef773e3429de
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-17 19:18:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
 
     @user=User.find_by(username: params[:username])
-    
+
     if @user != nil
       if @user.password == params[:password]
         session[:user_id]= @user.id

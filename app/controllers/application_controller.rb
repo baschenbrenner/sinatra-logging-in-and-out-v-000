48cc6ca4ed7607fd55a43a478ba52c02c6adbb5c
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-17 19:18:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    if Helper.is_logged_in?(session)
     @user =  User.find(session[:user_id])
     erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
